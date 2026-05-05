9855c2fcce2bee96d4fafadb778761eb71e987ac
Carolina <carolina.warntorp@outlook.com>
2026-05-05 09:34:46 +0200

    ref add theme logos

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
-import logo from "/logo.png";
+import logoblk from "/logo-blk.png";
+import logowht from "/logo-wht.png";
 
 interface ILogoProps {
   position: "frontpage" | "nav";
@@ -8,8 +9,16 @@ export const Logo = ({ position }: ILogoProps) => {
   return (
     <>
       <img
-        src={logo}
+        src={logoblk}
         id={`logo-${position}`}
+        className="logo-dark"
+        alt="who's that pkmn logo"
+        width={position === "nav" ? 60 : 300}
+      />
+      <img
+        src={logowht}
+        id={`logo-${position}`}
+        className="logo-light"
         alt="who's that pkmn logo"
         width={position === "nav" ? 60 : 300}
       />
