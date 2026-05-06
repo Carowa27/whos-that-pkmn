cd0685d2b13a6643029935891438c1d301aa6628
Carolina <carolina.warntorp@outlook.com>
2026-05-06 09:45:04 +0200

    bug after interface change

--- src/components/PkmnClue.tsx
+++ src/components/PkmnClue.tsx
@@ -8,7 +8,7 @@ interface IPkmnClueProps {
 }
 
 export const PkmnClue = ({ typeOfClue, pkmn, reveal }: IPkmnClueProps) => {
-  if ("dexEntries" in pkmn) {
+  if (pkmn.dexEntries !== null) {
     const textTrim = (text: string) => {
       const trimmedText = text.replace(/\f/g, " ").trim().split("\n").join(" ");
       return trimmedText;
