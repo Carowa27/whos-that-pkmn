5db90df7ab55bb2ea719acedaa80b33d3258c407
Carolina <carolina.warntorp@outlook.com>
2026-05-07 22:36:04 +0200

    ref add error handling on pkmnlist

--- src/pages/PkmnList.tsx
+++ src/pages/PkmnList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Pkmn } from "../components/Pkmn";
 import type { pkmn } from "../types/pkmn";
@@ -20,20 +21,27 @@ export const PkmnList = ({
     <>
       <Header />
       <h3>Your correct guesses</h3>
-      <ul id="pkmn-list">
-        {correctGuesses.map((p: pkmn) => (
-          <li key={p.id + "-" + p.pkmnName} id="correct-guess-wrapper">
-            <Pkmn pkmn={p} reveal={true} />
-            <span
-              id="trash-icon"
-              className="button icon-btn"
-              onClick={() => removeGuessFromState(p)}
-            >
-              ✕
-            </span>
-          </li>
-        ))}
-      </ul>
+      {correctGuesses.length === 0 ? (
+        <p>
+          You dont have any correct guesses, go to <Link to={"/"}>Home</Link> to
+          specify how you wanna play to start your journey!
+        </p>
+      ) : (
+        <ul id="pkmn-list">
+          {correctGuesses.map((p: pkmn) => (
+            <li key={p.id + "-" + p.pkmnName} id="correct-guess-wrapper">
+              <Pkmn pkmn={p} reveal={true} />
+              <span
+                id="trash-icon"
+                className="button icon-btn"
+                onClick={() => removeGuessFromState(p)}
+              >
+                ✕
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
